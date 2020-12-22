48150345cc6ff45e94a824350a9215b8ee00ed31
fhulu84 <fhulu84@users.noreply.github.com>
2020-12-22 08:47:46 +0000

    Day22 Challenge - Extract Matrix Column

---
+++ day22.js
@@ -0,0 +1,11 @@
+function extractMatrixColumn(matrix, column) {
+  return matrix.map(c => c[column]);
+  // const result = [];
+  // for(let i=0; i<matrix.length; i++) {
+  //   result.push(matrix[i][column])
+  // }
+  // return result;
+}
+
+console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
+console.group(extractMatrixColumn([],1));
